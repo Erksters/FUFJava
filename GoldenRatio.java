0108e4165aa4ee866fd02a4b143b60bd5a859320
Erick PC <erksters@gmail.com>
2021-04-29 22:24:20 -0500

    Added a bit more info to the print out statement for the golden ratio

--- GoldenRatio.java
+++ GoldenRatio.java
@@ -6,16 +6,19 @@
  *  Then run in your command line with:
  * 
  *      java GoldenRatio <value>
+ *
+ *  Happy coding!
  * 
  * @author Erick Saenz-Gardea
  * @version 1.0
  * @since 2021-04-29
  */
 
-/**
- * 
- */
 public class GoldenRatio{
+    
+    /**Global variables used to calculate the golden ratio
+     * @see calcRatio() 
+     */
     static double a = 0;
     static double b = 1;
 
@@ -27,55 +30,7 @@ public class GoldenRatio{
         calcRatio();        
     }
 
-
-    private static void PrintSequenceValues(){
-        System.out.print("The second to last value in the sequence is: ");
-        System.out.println(a);
-        System.out.print("The last value in the sequence is ");
-        System.out.println(b);
-    }
-
-    /**
-     * This method will set the global variables "a" and "b" to their respective values within 
-     * the fibonacci sequence
-     * Example:
-     *          0, 1, 1, 2 ,3 ,4
-     *                   a  b
-     * @param seqLength
-     */
-    private static void findLastTwoValues(int seqLength){
-        double c;
-        while(seqLength - 2 > 0){
-            c = a + b;
-            a = b;
-            b = c;
-            seqLength--;
-        }
-    }
-
-    /**
-     * This method determines if the sequence value in in range
-     *      where n > 1
-     * If the sequence is not in range then the program will exit 
-     * @param arg
-     */
-    private static void sequenceCheck(int arg){
-        if(arg < 2){
-            System.out.println("*********************** Uh oh ***********************");
-            System.out.println("Please enter an integer greater than 1 next time!");
-            System.out.println("Exiting!");
-            System.out.println("*********************** Uh oh ***********************");
-            System.exit(0);
-        }        
-
-        if(arg == 2){
-            System.out.println("*********************** Infinity Warning ***********************");
-            System.out.println("You are dividing by 0!");
-            System.out.println("*********************** Infinity Warning ***********************");
-        }
-    }
-
-    /**
+   /**
      * This method will extract the user-specified sequence in from the command line
      * @param cmdArg the string array from the command line
      * @exception NumberFormatException is caused by either trying to parse non-numeric charachters or integers larger than 2147483648
@@ -104,10 +59,62 @@ public class GoldenRatio{
     }
 
     /**
+     * This method determines if the sequence value in in range
+     *      where n > 1
+     * If the sequence is not in range then the program will exit 
+     * @param arg
+     */
+    private static void sequenceCheck(int arg){
+        if(arg < 2){
+            System.out.println("*********************** Uh oh ***********************");
+            System.out.println("Please enter an integer greater than 1 next time!");
+            System.out.println("Exiting!");
+            System.out.println("*********************** Uh oh ***********************");
+            System.exit(0);
+        }        
+
+        if(arg == 2){
+            System.out.println("*********************** Infinity Warning ***********************");
+            System.out.println("You are dividing by 0!");
+            System.out.println("*********************** Infinity Warning ***********************");
+        }
+    }
+
+    /**
+     * This method will set the global variables "a" and "b" to their respective values within 
+     * the fibonacci sequence
+     * Example:
+     *          0, 1, 1, 2 ,3 ,4
+     *                   a  b
+     * @param seqLength the command line integer used to increment the global variables correctly
+     */
+    private static void findLastTwoValues(int seqLength){
+        double c;
+        while(seqLength - 2 > 0){
+            c = a + b;
+            a = b;
+            b = c;
+            seqLength--;
+        }
+    }
+
+     /**
+     * This method will print of the values that are being used
+     * to calculate the golden ratio
+     */
+    private static void PrintSequenceValues(){
+        System.out.print("The second to last value in the sequence is: ");
+        System.out.println(a);
+        System.out.print("The last value in the sequence is ");
+        System.out.println(b);
+    }
+
+    /**
      * This method will print and determine the ratio between the second to last number
      * and the last number in the sequence given
      */
     private static void calcRatio(){
+        System.out.print("The golden ratio is: ");
         System.out.println(b / a);
     }
 }
